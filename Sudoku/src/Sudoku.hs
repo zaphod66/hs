89211e94a75832145652e048f2e51c49336a0da9
Patrick Roemer <quellkristfalconer@gmail.com>
2014-11-28 15:22:30 +0100

    col/row/square vals

--- Sudoku/src/Sudoku.hs
+++ Sudoku/src/Sudoku.hs
@@ -5,10 +5,12 @@ module Sudoku where
 import Test.QuickCheck
 import Test.QuickCheck.All
 import Test.QuickCheck.Gen
+import Data.Maybe
 
 runTests = $quickCheckAll
 
 boardSize = 9
+squareSize = 3
 
 type Field = Maybe Int
 
@@ -19,16 +21,34 @@ data Pos = Pos Int Int deriving Show
 posGen :: Gen Pos
 posGen =
   do
-    x <- choose (0, boardSize - 1)
-    y <- choose (0, boardSize - 1)
-    return (Pos x y)
+    r <- choose (0, boardSize - 1)
+    c <- choose (0, boardSize - 1)
+    return (Pos r c)
 
 instance Arbitrary Pos where
   arbitrary = posGen
 
 field :: Board -> Pos -> Field
-field b (Pos x y) = b !! ((x * boardSize) + y)
+field b (Pos r c) = b !! ((r * boardSize) + c)
 
-prop_field pos @ (Pos x y) =
-  field board pos == (Just $ x * boardSize + y + 1)
+prop_field pos @ (Pos r c) =
+  field board pos == (Just $ r * boardSize + c + 1)
   where board = map (\i -> Just i) [1..81]
+
+rowVals :: Board -> Pos -> [Int]
+rowVals b (Pos r c) = catMaybes [field b (Pos r c') | c' <- take boardSize [0..]]
+
+colVals :: Board -> Pos -> [Int]
+colVals b (Pos r c) = catMaybes [field b (Pos r' c) | r' <- take boardSize [0..]]
+
+squareVals :: Board -> Pos -> [Int]
+squareVals b (Pos r c) =
+  catMaybes [field b (Pos r' c') | r' <- take squareSize [(off r)..]
+                                 , c' <- take squareSize [(off c)..]]
+  where off x = x `div` squareSize * squareSize
+
+
+
+-- free :: Board -> [Pos]
+
+-- update :: Board -> Pos -> Int -> Board
