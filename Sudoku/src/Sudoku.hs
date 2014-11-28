4862b6c7c304989ec655b95e7fd07946e0164b52
Patrick Roemer <quellkristfalconer@gmail.com>
2014-11-28 16:18:50 +0100

    tests for row/col values, possibleVals and allPossibleVals

--- Sudoku/src/Sudoku.hs
+++ Sudoku/src/Sudoku.hs
@@ -6,6 +6,7 @@ import Test.QuickCheck
 import Test.QuickCheck.All
 import Test.QuickCheck.Gen
 import Data.Maybe
+import Data.List
 
 runTests = $quickCheckAll
 
@@ -32,14 +33,24 @@ field :: Board -> Pos -> Field
 field b (Pos r c) = b !! ((r * boardSize) + c)
 
 prop_field pos @ (Pos r c) =
-  field board pos == (Just $ r * boardSize + c + 1)
-  where board = map (\i -> Just i) [1..81]
+  field board pos == (Just $ r * boardSize + c)
+  where board = map (\i -> Just i) (take (boardSize ^ 2) [0..])
 
 rowVals :: Board -> Pos -> [Int]
 rowVals b (Pos r c) = catMaybes [field b (Pos r c') | c' <- take boardSize [0..]]
 
+prop_rowVals pos @ (Pos r _) =
+  sort rv == take boardSize [(r * boardSize)..]
+  where board = map (\i -> Just i) (take (boardSize ^ 2) [0..])
+        rv = rowVals board pos
+
 colVals :: Board -> Pos -> [Int]
 colVals b (Pos r c) = catMaybes [field b (Pos r' c) | r' <- take boardSize [0..]]
+
+prop_colVals pos @ (Pos _ c) =
+  sort cv == [v * boardSize + c | v <- take boardSize [0..]]
+  where board = map (\i -> Just i) (take (boardSize ^ 2) [0..])
+        cv = colVals board pos
 
 squareVals :: Board -> Pos -> [Int]
 squareVals b (Pos r c) =
@@ -47,7 +58,21 @@ squareVals b (Pos r c) =
                                  , c' <- take squareSize [(off c)..]]
   where off x = x `div` squareSize * squareSize
 
+-- TODO test squareVals
 
+possibleVals :: Board -> Pos -> [Int]
+possibleVals b pos = [1..boardSize] \\ concat [ext b pos | ext <- [rowVals, colVals, squareVals]]
+
+idxToPos :: Int -> Pos
+idxToPos i = Pos (i `div` boardSize) (i `mod` boardSize)
+
+allPossibleVals :: Board -> [(Pos, Maybe [Int])]
+allPossibleVals b = [(pos, (possVals b pos)) | pos <- positions]
+  where positions = [Pos r c | r <- take boardSize [0..], c <- take boardSize [0..]]
+        possVals :: Board -> Pos -> Maybe [Int]
+        possVals b pos = case (field b pos) of
+                           Just _ -> Nothing
+                           Nothing -> Just $ possibleVals b pos
 
 -- free :: Board -> [Pos]
 
